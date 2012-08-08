7acbca16010115ffcb43f4fe95ccd8c56a628625
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2012-08-08 21:40:25 +0200

    Add Mustache Templating

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
+error_reporting(E_ALL); 
 $page=1;
 $title = "Emmanuel DEMEY - Spécialiste du WEB dans la Région Lilloise - Mon Parcours";
 $description = "Grâce à ce document, vous trouverez tout mon parcours professionnel et universitaire, avec notamment tous les projets sur lesquels j'ai travaillé.";
 include "header.php";
+
+
+require 'mustache/src/Mustache/Autoloader.php';
+Mustache_Autoloader::register();
+
+$m = new Mustache_Engine;
+$array = json_decode(file_get_contents('data.json'));
 ?>
 			
 
@@ -24,65 +32,9 @@ include "header.php";
 					<!-- content -->
 					<div id="content">
 						<h4 class="line-divider">Expériences Professionnelles</h4>
-						<h5>2010 : <a href="http://fr.atos.net/fr-fr/" target="_blank">Atos (Seclin)</a> - Ingénieur Etude & Développement</h5>
-						
-					
-						<ul>
-							<li>Membre de l'équipe spécialisée dans les technologies WEB</li>
-							<li>Formateur Tapestry (Seclin, Lyon & Barcelonne)</li>
-							<li>Responsable de projets open-source</li>
-							<li>Participation à la communauté Tapestry : support, mailing-list ...</li>
-							<li class="competence"><strong>Technologies : Tapestry, Maven, Java, jQuery, HTML5</strong></li>
-						</ul>
-						
-						<h5>2009 - 2010 : <a href="http://www.mines-douai.fr/" target="_blank">Ecole des Mines de Douai</a> - Ingénieur Développement</h5>
-						
-				
-							<ul>
-								<li>Développement d'Applications Ecole : Gestion des Frais de Déplacement</li>
-								<li class="competence"><strong>Technologies : HTML, PHP, JavaScript, MySQL, ExtJS</strong></li>
-							</ul>
-			
-						
-						<h5>2009 : <a href="http://www.hestia-france.com/" target="_blank">Hestia France (Templeuve)</a> - Stagiaire Analyste Développeur</h5>
-						
-							<ul>
-								<li>En charge de l'intégration des protocoles SNMP et Syslog au sein des centrales de domotique.</li>
-								<li>Développement du firmware du composant XPort de Lantronix, en langage C.</li>
-								<li>Amélioration d'applications existantes en C++ et Java.</li>
-								<li class="competence"><strong>Technologies : C, C++, Java</strong></li>
-							</ul>
-						
-						<h5>2007 - 2008 : <a href="http://www.akebonobrakes.com/" target="_blank">Akebono (Monchy-le-Preux)</a> - Stagiaire Ingénieur Développeur</h5>
-						
-						<ul>
-							<li>En charge du développement d'une application de supervision des lignes de production, 
-							afin de récupérer automatiquement les données importantes (production et rebuts) et 
-							les utiliser directement dans l'ERP.</li>
-							
-							<li class="competence">
-								<strong>Technologies : Visual Basic .NET, SQL Server 2005</strong>
-							</li>
-						</ul>
-						
-						
-						<h5>2007 : <a href="http://www.oxfam.org/fr" target="_blank">Oxfam (Bromley, Angleterre)</a> - Stagiaire Responsable de Magasin</h5>
-						
-						<ul>
-							<li>Stage linguistique. Gestion de Magasin et d'un équipe.</li>
-						</ul>
-						
-						<h5>2006 : <a href="http://www.samk.fi/english/" target="_blank">Satakunta University (Rauma, Finlande)</a> - Stagiaire Technicien Développeur</h5>
-						
-						<ul>
-							
-							<li>Responsable du développement d'une plateform d'E-learning.</li>
-							
-							<li class="competence">
-								<strong>Technologies : HTML, CSS, PHP, MySQL</strong>
-							</li>
-						</ul>
-						
+						<?php
+							print $m->render(file_get_contents('template.mustache'), $array);
+						?>
 						
 						<h4 class="line-divider">Etudes</h4>
 						
@@ -95,7 +47,7 @@ include "header.php";
 							<li>Génie Industriel : Electronique, Electronique de Puissance, Automatisme, Automatique</li>
 						</ul>	
 						
-						<h5>2004 - 206 : <a href="http://www.iutenligne.net/IUT/Valenciennes" target="_blank">IUT Génie Electrique et Informatique Industrielle (Valenciennes)</a></h5>
+						<h5>2004 - 2006 : <a href="http://www.iutenligne.net/IUT/Valenciennes" target="_blank">IUT Génie Electrique et Informatique Industrielle (Valenciennes)</a></h5>
 						
 						<ul>
 							<li>Options Réseaux Locaux Industriels</li>
@@ -103,6 +55,14 @@ include "header.php";
 							<li>Génie Industriel : Electronique, Electronique de Puissance, Automatisme</li>
 						</ul>
 						
+						
+						<h4 class="line-divider">Projets Personnels</h4>
+						
+						<ul>
+							<li>Administrateur du site <a href="http://brassband-npdc.fr/" target="_blank">http://brassband-npdc.fr/</a> (Joomla)</li>
+							<li>Administrateur du site <a href="http://emmanueldemey.fr/" target="_blank">http://emmanueldemey.fr/</a> (PHP, CSS, Mustache)</li>
+							<li>Voyages</li>
+						</ul>
 						
 						<h4 class="line-divider">Loisirs</h4>
 						
