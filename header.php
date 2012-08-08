db56009b2661dc79ea222341df67f09328fdc6a9
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2012-08-08 21:55:04 +0200

    Add Viewport meta tag

--- header.php
+++ header.php
@@ -2,7 +2,10 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-	        <meta name="description" content="<?php echo $description; ?>">	
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"></meta>
+		<meta content="width=device-width, initial-scale=1.0" name="viewport"></meta>
+	    <meta name="description" content="<?php echo $description; ?>">	
 		<title><?php echo $title;?></title>
 		<!-- CSS -->
 		<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
